5d067c19c81c782a3fe4d090d7ab8b6140fc97cc
mc-ridge <mc.ridgeway2004@gmail.com>
2025-02-12 14:44:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
+
+'''
+(this is what I am testing w in main.py)
+
+
 from PlayerDatabase import PlayerDatabase
 
-
 class main:
-    
     def __init__(self):
-        # Update credentials here
-        self.db = PlayerDatabase(
-            dbname="photon", 
-            user="student", 
-            password="student", 
-            host="localhost"
-            #'port': 5432
-        )
+        self.db = PlayerDatabase()  # No need to pass credentials!
 
     def run(self):
+        # Adding players
         self.db.add_player(500, 'BhodiLi')
+        self.db.add_player(501, 'Alpha')
+
+        # Retrieving player info
         print("Codename for ID 500:", self.db.get_codename_by_id(500))
+        print("ID for codename 'Alpha':", self.db.get_id_by_codename('Alpha'))
+
+        # Close DB connection
         self.db.close_connection()
 
+# Run the application
 if __name__ == "__main__":
     app = main()
     app.run()
+
+
+'''
