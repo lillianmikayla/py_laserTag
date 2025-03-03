0d84bc8335fdec8d8461599a7d37f30e61a93cff
Sophmin05 <158496669+Sophmin05@users.noreply.github.com>
2025-03-03 14:06:03 -0600

    Cleaning up code and comments
    Removed excess comments and dead code, changed id & codename callback names for clarity.

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import threading
 #git pull origin
 
 #player DB class, previously class main in the database main.py test file
+#commented out when not testing in VM
+
 # class PlayerDBApp:
 #     def __init__(self):
 #         self.db = PlayerDatabase()  # No need to pass credentials!
@@ -85,12 +87,9 @@ class fakeDatabase:
         self.localPlayerCount += 1
         #udpclient.player_added(self.localPlayerCount)
 
-#callback for ID handling, app_data = ID input
-def input_int_callback(sender, app_data, user_data):
-    info = str(app_data)
-    #print(f"Input from Row {user_data[0]}, Column {user_data[1]}: {info}")
-    #print(f"Sender: {sender}")
-    
+#callback section: sender = table cell ID, app_data = value in cell, user_data = tuple of (row, column, app)
+
+def input_id_callback(sender, app_data, user_data):
     #invalid theme for handling invalid input scenario
     with dpg.theme() as invalid_theme:
         with dpg.theme_component(dpg.mvAll):
@@ -105,6 +104,7 @@ def input_int_callback(sender, app_data, user_data):
     else:
         appAcc = user_data[2]
         check = appAcc.checkID(app_data)
+        #case where ID is not in database
         if check == None:
             if (sender == f"redTable_{user_data[0]}"):
                 dpg.configure_item(f"redTable_codename_{user_data[0]}", readonly=False, hint="Enter Codename")
@@ -115,6 +115,7 @@ def input_int_callback(sender, app_data, user_data):
                 dpg.configure_item(f"greenTable_equipment_{user_data[0]}", readonly=False)
                 dpg.bind_item_theme(sender, 0)
             return
+        #ID is in database
         else:
             if sender == f"redTable_{user_data[0]}":
                 # Set the codename to the value stored in check
@@ -127,16 +128,7 @@ def input_int_callback(sender, app_data, user_data):
                 dpg.bind_item_theme(sender, 0)
             return
         
-        #userdata is row,col indicies of the cell, appdata is the actual contents
-        #we can use this to send information into the database
-        
-        #might need to update input checking
-
-#callback for CODENAME handling, app_data = codename input
-def input_text_callback(sender, app_data, user_data):
-    #print(f"Input from Row {user_data[0]}, Column {user_data[1]}: {app_data}")
-    #print(f"Sender: {sender}")
-    
+def input_codename_callback(sender, app_data, user_data):
     idValue = None
     if "redTable" in sender:
         idValue = dpg.get_value(f"redTable_{user_data[0]}")
@@ -146,15 +138,10 @@ def input_text_callback(sender, app_data, user_data):
     appAcc.addPlayer(idValue, app_data)
     
 def input_equipID_callback(sender, app_data, user_data):
-    #callback for equipment ID input
     udpclient.player_added(app_data)
 
-#Callback for swapping network, app_data = IP Address input
 def network_swap_callback(sender, app_data, user_data):
-    #print(f"Input: {app_data}")
-    #print(f"Sender: {sender}")
-
-    #further modification to update network address here
+    #further modification to handle bad IP needed (?)
     udpclient.change_network(app_data)
     udpserver.change_network(app_data)
 
@@ -165,9 +152,6 @@ def show_main_window(app):
     with dpg.window(tag="RedTable", pos=(0, 0), width=450, height=400,no_title_bar=True, no_resize=True, no_move=True, no_scrollbar=True) as redTeam:
         dpg.add_text("Red Team", parent=redTeam)
         with dpg.table(header_row=True, label = "Red Team") as table_red_id:
-
-            # use add_table_column to add columns to the table,
-            # table columns use child slot 0
             dpg.add_table_column(label = "Player Number", width_fixed=True,width=100)
             dpg.add_table_column(label = "Player ID", width_fixed=True,width=80)
             dpg.add_table_column(label = "Player Codename", width_fixed=True,width=180)
@@ -179,9 +163,9 @@ def show_main_window(app):
                         if j == 0:
                             dpg.add_text(f"Player {i}")
                         elif j == 1:
-                            dpg.add_input_int(callback=input_int_callback, user_data=(i, j, app), on_enter=True, parent=row_red_id, step=0, step_fast=0, width = 80, tag=f"redTable_{i}")
+                            dpg.add_input_int(callback=input_id_callback, user_data=(i, j, app), on_enter=True, parent=row_red_id, step=0, step_fast=0, width = 80, tag=f"redTable_{i}")
                         elif j == 2:
-                            dpg.add_input_text(callback=input_text_callback, user_data=(i, j, app), on_enter=True, parent=row_red_id, hint="Awaiting ID", width = 150, readonly=True, tag=f"redTable_codename_{i}")
+                            dpg.add_input_text(callback=input_codename_callback, user_data=(i, j, app), on_enter=True, parent=row_red_id, hint="Awaiting ID", width = 150, readonly=True, tag=f"redTable_codename_{i}")
                         elif j == 3:
                             dpg.add_input_int(callback=input_equipID_callback, user_data=(i, j, app), on_enter=True, parent=row_red_id, step=0, step_fast=0, readonly=True, width = 80, tag=f"redTable_equipment_{i}")
 
@@ -189,9 +173,6 @@ def show_main_window(app):
     with dpg.window(tag="GreenTable", pos=(450, 0), width=450, height=400,no_title_bar=True, no_resize=True, no_move=True, no_scrollbar=True) as greenTeam:
         dpg.add_text("Green Team", parent=greenTeam)
         with dpg.table(header_row=True, label = "Green Team") as table_green_id:
-
-            # use add_table_column to add columns to the table,
-            # table columns use child slot 0
             dpg.add_table_column(label = "Player Number", width_fixed=True,width=100)
             dpg.add_table_column(label = "Player ID", width_fixed=True,width=80)
             dpg.add_table_column(label = "Player Codename", width_fixed=True,width=180)
@@ -203,9 +184,9 @@ def show_main_window(app):
                         if j == 0:
                             dpg.add_text(f"Player {i}")
                         elif j == 1:
-                            dpg.add_input_int(callback=input_int_callback, user_data=(i, j, app), on_enter=True, parent=row_green_id, step=0, step_fast=0, width = 80, tag=f"greenTable_{i}")
+                            dpg.add_input_int(callback=input_id_callback, user_data=(i, j, app), on_enter=True, parent=row_green_id, step=0, step_fast=0, width = 80, tag=f"greenTable_{i}")
                         elif j == 2:
-                            dpg.add_input_text(callback=input_text_callback, user_data=(i, j, app), on_enter=True, parent=row_green_id, hint="Awaiting ID", width = 150, readonly=True, tag=f"greenTable_codename_{i}")
+                            dpg.add_input_text(callback=input_codename_callback, user_data=(i, j, app), on_enter=True, parent=row_green_id, hint="Awaiting ID", width = 150, readonly=True, tag=f"greenTable_codename_{i}")
                         elif j == 3:
                             dpg.add_input_int(callback=input_equipID_callback, user_data=(i, j, app), on_enter=True, parent=row_green_id, step=0, step_fast=0, readonly=True, width = 80, tag=f"greenTable_equipment_{i}")
 
@@ -217,8 +198,6 @@ def show_main_window(app):
         with dpg.theme_component(dpg.mvAll):
             dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (120, 120, 120), category=dpg.mvThemeCat_Core) 
             dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core)
-            
-        #with dpg.theme_component(dpg.mvTableHeaderRow):
             dpg.add_theme_color(dpg.mvThemeCol_TableHeaderBg, (100, 50, 50), category=dpg.mvThemeCat_Core)
 
     dpg.bind_item_theme(redTeam, red_theme)
@@ -231,8 +210,6 @@ def show_main_window(app):
         with dpg.theme_component(dpg.mvAll):
             dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (120, 120, 120), category=dpg.mvThemeCat_Core) 
             dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core) 
-            
-        #with dpg.theme_component(dpg.mvTableHeaderRow):
             dpg.add_theme_color(dpg.mvThemeCol_TableHeaderBg, (50, 100, 50), category=dpg.mvThemeCat_Core) 
 
     dpg.bind_item_theme(greenTeam, green_theme)
@@ -282,12 +259,6 @@ def main():
     #app = PlayerDBApp()
     app = fakeDatabase()
     
-    #resets database, adds ID 500 and 501
-    #app.flush()
-    #app.runTest()
-    #print("Add players to database via the GUI - can also unncomment the runTest() function to add players to the database directly")
-    #print("\nEquipment ID is input via the console\n")
-    
     #first, initial loop - splash screen
     while dpg.is_dearpygui_running():
         current_time = time.time()
@@ -296,12 +267,10 @@ def main():
             show_main_window(app)
             break
         dpg.render_dearpygui_frame()
-        #print("im loop 1\n")
 
     #second, regular loop (continuously ran)
     while dpg.is_dearpygui_running():
         dpg.render_dearpygui_frame()
-        #print("im loop 2\n")
 
     dpg.destroy_context()
 
